e578e3c2fc0bd64cecb744a1b19c0b1788ba5269
joerifat <joerifat6@gmail.com>
2025-11-23 19:05:58 +0200

    Create main_scraping.py

---
+++ main_scraping.py
@@ -0,0 +1,27 @@
+from agentstate import AgentState
+from web_logic import extract_from_web, save_extracted_result, save_extracted_result_db
+
+if __name__ == "__main__":
+
+    state: AgentState = {
+        "config": {
+            "pagination": {
+                "enabled": True,
+                "start": 1,
+                "end": 3
+            },
+            "parent_tag": "article",
+            "parent_class": "product_pod",
+            "fields": {
+                "title": ["h3", "text"],
+                "price": ["p", "text"],
+                "link": ["a", "href"]
+            }
+        }
+    }
+
+    result = extract_from_web(state)
+    print(result)
+
+    save_extracted_result(result["extracted_data"])
+    save_extracted_result_db(result["extracted_data"])
