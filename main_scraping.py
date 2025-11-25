f13d2deb4051edfdbeb2dbbac50a2efe888eb282
youssef rifat <joerifat6@gmail.com>
2025-11-26 00:06:51 +0200

    Update main_scraping.py

--- main_scraping.py
+++ main_scraping.py
@@ -3,32 +3,38 @@ from web_logic import extract_from_web, save_extracted_result, save_extracted_re
 
 if __name__ == "__main__":
 
-    state: AgentState = {
-    "urls": [
-        {
-            "url": "https://books.toscrape.com/catalogue/page-{}.html",
-            "doc_type": "html"
-        }
-    ],
-    "config": {
-        "pagination": {
-            "enabled": True,
-            "start": 1,
-            "end": 3
-        },
-        "parent_tag": "article",
-        "parent_class": "product_pod",
-        "fields": {
-            "title": ["h3", "text"],
-            "price": ["p", "text"],
-            "link": ["a", "href"]
+    # --- Define scraping state ---
+    state = {
+        "urls": [
+            {
+                "url": "https://books.toscrape.com/catalogue/page-{}.html",
+                "doc_type": "html"
+            }
+        ],
+        "config": {
+            "pagination": {
+                "enabled": True,
+                "start": 1,
+                "end": 3
+            }
         }
     }
-}
 
+    
+    wait_selector = None  
+   
+    infinite_scroll = False
 
-    result = extract_from_web(state)
-    print(result)
+   
+    result = extract_from_web(
+        state,
+        wait_selector=wait_selector,
+        infinite_scroll=infinite_scroll
+    )
 
+  
     save_extracted_result(result["extracted_data"])
+
+  
     save_extracted_result_db(result["extracted_data"])
+
