29122e51c9162f51c92dae689af7b27a9034faa9
Ismail Al-hetimi <146815298+IsmailMohamed010@users.noreply.github.com>
2025-12-14 00:01:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from agentstate import AgentState
-from logic import extract_files, save_extracted_result, save_extracted_result_db
+from detect_excel_relations import detect_excel_relationships, print_detected_relationships
+from save_files import save_extracted_result_db, save_extracted_result_csv, save_relations_json
+from extract_files import extract_files
 
-if __name__ == "__main__":
-    state: AgentState = {
-        "files": [
-            {"file_path": "test_data/amazon.csv", "doc_type": "file"},
-            {"file_path": "test_data/multi_sheet_demo.xlsx", "doc_type": "file"}
-        ]
-    }
+state: AgentState = {
+    "files": [
+        {"file_path": "test_data/multi_sheet_demo.xlsx", "doc_type": "file"},
+        {"file_path": "test_data/multi_sheet_demo - Copy.xlsx", "doc_type": "file"},
+    ]
+}
 
-    result = extract_files(state)
-    print(result)
-    
-    save_extracted_result(result["extracted_data"])
-    save_extracted_result_db(result["extracted_data"])
+result = extract_files(state)
+
+if "extracted_data" not in result or not result["extracted_data"]:
+    print("❌ No data extracted. Exiting.")
+    exit(1)
+
+extracted_data = result["extracted_data"]
+
+state_relationship = detect_excel_relationships({"extracted_data": extracted_data})
+print_detected_relationships(state_relationship)
+save_relations_json(state_relationship.get("detected_relations", {}))
+save_extracted_result_csv(extracted_data)
+
+save_extracted_result_db(extracted_data)
